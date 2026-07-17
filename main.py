ed9c6b59e1309c6078ad2c6be1e7beb10709196a
vipinvs1 <vipin.v1@tataelxsi.co.in>
2026-07-17 16:04:57 +0530

    starte application

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI(title="Python Inventory API")
+app = FastAPI()
+
+products = []
+
 
 class Product(BaseModel):
     id: int
     name: str
 
-products = [
-    {"id": 101, "name": "Laptop"}
-]
 
 @app.get("/health")
 def health():
-    return {
-        "status": "UP",
-        "app": "Python Inventory API"
-    }
+    return {"status": "UP"}
+
 
 @app.get("/products")
 def get_products():
     return products
 
+
 @app.post("/products")
 def add_product(product: Product):
     products.append(product.dict())
-    return product
+    return {
+        "message": "Product added successfully",
+        "product": product
+    }
